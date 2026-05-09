0196ab7c860b33ca6149bee3e81ce74bea15a902
martingerardoc <martingerardoc56@gmail.com>
2026-05-09 19:34:06 -0300

    week 01 arrays assigment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // Create a new array with the size given by 'length'
 
-        return []; // replace this return statement with your own
+        double [] multiples = new double[length];
+       
+       // Use a loop to go through each position in the array
+       for (int i = 0; i < length; i++)
+       {
+        // Calculate the multiple of the number
+        multiples[i] = number * (i + 1);
+       }
+
+       // return the completed array
+       return multiples;
     }
 
     /// <summary>
@@ -29,5 +40,20 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Find the index where rotation need to start
+        int starIndex = data.Count - amount;
+
+        // creating a temporary list containing the last
+
+        List<int> temp = data.GetRange(starIndex, amount);
+
+        //remove the last amount elements from the original list
+
+        data.RemoveRange(starIndex, amount);
+
+        //Inserting the temporary list at the beginning
+
+        data.InsertRange(0 , temp);
     }
 }
